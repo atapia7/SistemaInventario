a64f3f029fcafc2ffa0732926381ac49d94c11fa
atapia7 <michaeltapialmidon07@gmail.com>
2024-10-18 05:35:20 -0500

    [#][ADD]. Envío de Emails usando SendGrid para usuarios registrados | confirmEmail.cshtml

--- SistemaInventario.Utilidades/EmailSender.cs
+++ SistemaInventario.Utilidades/EmailSender.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +14,24 @@ namespace SistemaInventario.Utilidades
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public string SendGridSecret { get; set; }
+        private readonly IConfiguration _config;
+
+        public EmailSender(IConfiguration config)
         {
+            _config = config;
+            SendGridSecret = _config.GetValue<string>("Sendgrid:SecretKey");
+        }
+
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //var client = new SendGridClient(SendGridSecret);
+            //var from = new EmailAddress("michaeltapialmidon07@gmail.com");
+            //var to=new EmailAddress(email);
+            //var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+            //return client.SendEmailAsync(msg);
             throw new NotImplementedException();
         }
     }
--- SistemaInventario/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SistemaInventario/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.WebUtilities;
 using SistemaInventario.Modelos;
 using SistemaInventario.Utilidades;
 
@@ -156,6 +157,7 @@ namespace SistemaInventario.Areas.Identity.Pages.Account
                     }
 
                     var userId = await _userManager.GetUserIdAsync(user);
+
                     //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     //var callbackUrl = Url.Page(
@@ -163,9 +165,8 @@ namespace SistemaInventario.Areas.Identity.Pages.Account
                     //    pageHandler: null,
                     //    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                     //    protocol: Request.Scheme);
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirmar tu email", $"Confirmar tu cuenta <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
